dfd668c54e35b899f357e4a83f8236670dbd4742
Harry <gexaris1@gmail.com>
2023-06-09 13:04:38 +0200

    initialize store

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -7,7 +7,7 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (
+const repositoriesReducer = (
   state: RepositoriesState,
   action: Action
 ): RepositoriesState => {
@@ -24,4 +24,4 @@ const reducer = (
   }
 };
 
-export default reducer;
+export default repositoriesReducer;
