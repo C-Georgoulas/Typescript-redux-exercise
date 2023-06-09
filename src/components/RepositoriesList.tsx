f43f1467f93d35de9caa09c56896047ccf091163
Harry <gexaris1@gmail.com>
2023-06-09 13:47:42 +0200

    omg

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+
 const RepositoriesList: React.FC = () => {
-    return <div>
-        <form>
-            <input />
-            <button>Search</button>
-        </form>
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    searchRepositories(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
     </div>
-}
+  );
+};
 
 export default RepositoriesList;
